4dc40e67847828c1e37492473976b4d03c9f73fe
mohitpanchal09 <panchalmohitg2002@gmail.com>
2023-11-21 21:56:29 +0530

    nodejs,process,linux commands, browser vs node ,argv

---
+++ Lecture-40/node-basic/index.js
@@ -0,0 +1,8 @@
+let pi = 3.14;
+let favNumber=7;
+console.log('samarth')
+function square(n){
+    return n*n
+}
+console.log(square(pi))
+console.log('hello')
---
+++ Lecture-40/process/app.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+console.log(process.argv)
+
+// let arr = process.argv.slice(2);
+// console.log(arr)
+// for(let item of arr){
+//     console.log(item)
+// }
+
+let el= process.argv.pop()
+console.log(el)
+for (let i =0;i<=el;i++){
+    console.log(i)
+}
