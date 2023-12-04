466320874c643cd2a77fec8aada415daef47fc9b
mohitpanchal09 <panchalmohitg2002@gmail.com>
2023-12-04 21:55:58 +0530

    express ,app,server,middleware,req,res,nodemon

---
+++ Lecture-45/express-basics/index.js
@@ -0,0 +1,21 @@
+
+
+const express = require('express'); //function
+// this app is the entire instance of your application
+const app = express(); // called the function and returned application object
+
+
+//middleware=>function
+// app.use(()=>{
+//     console.log('you made a request')
+// }) //accepts a cb function
+
+app.use((req,res)=>{
+    // console.log(req)
+    // console.log(res)
+    res.send('hi sever me response bhejdia')
+}) 
+
+app.listen(8080,()=>{
+    console.log('server connected at 8080')
+})
