17bf6a5d58acba070929d1bb8f5588134531060b
mohitpanchal09 <panchalmohitg2002@gmail.com>
2024-01-05 22:04:44 +0530

    clinet side, servider side validation by joi,cookie

---
+++ Lecture-57/Ecommerce-v1/routes/review.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router();
+const Review = require('../models/Review')
+const Product = require('../models/Product');
+const { validateReview } = require('../middleware');
+
+
+router.post('/products/:id/rating', validateReview ,async(req,res)=>{
+    try{
+
+    
+    let {id} = req.params
+    let {rating,comment} = req.body
+    let product = await Product.findById(id)
+    // new review
+    let review = new Review({rating,comment})
+    product.reviews.push(review)
+    await product.save()
+    await review.save()
+    res.redirect(`/product/${id}`)
+    }catch(e){
+        res.render('error',{err:e.message})
+    }
+})
+
+
+
+module.exports = router;
